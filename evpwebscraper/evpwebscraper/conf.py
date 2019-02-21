65de042dc7988737004b62c75c22f1532ac86733
hgibson1 <7027895+hgibson1@users.noreply.github.com>
2019-02-21 12:52:03 -0500

    Fixed table search so it would handle rows with items that were two columns wide

--- evpwebscraper/evpwebscraper/conf.py
+++ evpwebscraper/evpwebscraper/conf.py
@@ -1,7 +1,8 @@
 # Link to wikipedia page with list of municpalites in state
 #WIKI_LINK = 'https://en.wikipedia.org/wiki/List_of_municipalities_in_Massachusetts'
 #WIKI_LINK = 'https://en.wikipedia.org/wiki/List_of_municipalities_in_Florida'
-WIKI_LINK = 'https://en.wikipedia.org/wiki/List_of_counties_in_Florida'
+#WIKI_LINK = 'https://en.wikipedia.org/wiki/List_of_counties_in_Florida'
+WIKI_LINK = 'https://en.wikipedia.org/wiki/List_of_municipalities_in_Georgia_(U.S._state)'
 # Column number which contains municpality names
 # Different tables have names in different column,
 # For example massachusetts names are in 1st column and florida names are in second column
@@ -10,7 +11,8 @@ COLUMN = 0
 #COLUMN = 1
 # Abreviation for the state
 #STATE = 'ma'
-STATE = 'fl'
+#STATE = 'fl'
+STATE = 'ga'
 # Abreviation for search Query
-#SEARCH_FOR = 'town'
-SEARCH_FOR = 'county'
+SEARCH_FOR = 'town'
+#SEARCH_FOR = 'county'
--- evpwebscraper/evpwebscraper/spiders/wikisearch.py
+++ evpwebscraper/evpwebscraper/spiders/wikisearch.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 from string import printable
 from bs4 import BeautifulSoup
 
 from .. conf import *
 
-def element_filter(element):
+def filter_element(element):
     formated_element = ''
     try:
-        formated_element = element.get_text().split('[')[0].strip().replace(',', '')
+        formated_element = re.sub('([\n\t,]|\[\d\])*', '', element.get_text().strip())
     except IndexError: 
-        formated_element = element.find('a').get_text().split('[')[0].strip().replace(',', '')
+        formated_element = re.sub('([\n\t,]|\[\d\])*', '', element.find('a').get_text().strip())
     return ''.join(formated_element)
 
 class WikisearchSpider(scrapy.Spider):
@@ -41,15 +42,35 @@ class WikisearchSpider(scrapy.Spider):
         # Get table rows
         rows = table.find_all('tr')
         # Get table headers
-        headers = [th.get_text().split('[')[0].strip() for th in rows[0].find_all('th')]
+        pattern = re.compile('t(d|h)')
+        ths = rows[0].find_all(pattern)
+        headers = []
+        for th in ths:
+            headers.append(filter_element(th))
+            if th.get('colspan') and int(th.get('colspan')) > 1:
+                for i in range(int(th.get('colspan')) - 1): 
+                    # Will add spacing
+                    # If colspan is 2, range(2 - 1) = range(1) = [0], so '-' will be appended one time 
+                    headers.append('-') # Place holder
         FEED_EXPORT_FIELDS = headers
         print(','.join(headers))
+        # Get table data
         for row in rows[1:]:
-            elements = row.find_all('th')
-            elements.extend(row.find_all('td'))
+            elements = row.find_all(pattern)
             if len(elements) == 0:
+                # If there are no elements
                 continue
-            filtered_elements = [element_filter(e) for e in elements]
+            filtered_elements = []
+            for e in elements:
+                filtered_elements.append(filter_element(e))
+                if e.get('colspan') and int(e.get('colspan')) > 1:
+                    for i in range(int(e.get('colspan')) - 1): 
+                        # Will add spacing
+                        # If colspan is 2, range(2 - 1) = range(1) = [0], so '-' will be appended one time 
+                        filtered_elements.append('-') # Place holder
+            if len(filtered_elements) != len(headers):
+                # Skip line if length is incorrect since this will mess up output
+                continue
             print(','.join(filtered_elements))
             yield dict(zip(headers, filtered_elements)) 
 
